9beb5ce88c7aa12c4f81c20c9a18426f672aa14a
Savannahpolcen <Savannahpolcen@gmail.com>
2022-04-12 20:38:04 -0400

    setting varibles in JS

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var city="";
+
+// declare all variables 
+var searchCity = $('#search-city');
+var searchButton = $('#search-button');
+var currentCity = $('#current-city')
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
